0b67c8236e50010bfd2dc1d79135f1511b721dee
Iffy Books <96699073+iffybooks@users.noreply.github.com>
2022-04-08 09:43:43 -0400

    Added comments to Python script, updated README

--- 02_Python_and_Wget/LiveJournal_Backup_with_Python_and_Wget.py
+++ 02_Python_and_Wget/LiveJournal_Backup_with_Python_and_Wget.py
@@ -9,22 +9,31 @@ import random
 ## Change directory to the desktop
 os.chdir(os.path.expanduser('~/Desktop/'))
 
-## Initialize pagination value
+## Initialize pagination variable
 skip_value = 0
 
-###################################################
-## Choose a LiveJournal URL (replace with your own)
+############################################
+## Choose a LiveJournal URL
 home_url = "https://imomus.livejournal.com"
-###################################################
+############################################
 
 ## Removing slash from end of URL
 home_url = home_url.strip("/")
 
 ## Run the scrape
-# If your LJ has <20K posts, this script will grab everything (excluding comments)
+# If the LiveJournal account has <20K posts, this script will download every post (excluding comments)
 for i in range(1000): 
+    ## Generate current URL
 	current_page_url = f"{home_url}/?skip={skip_value}"
+    
+    ## Create Wget command as a list
 	wget_command_list = ["wget", "-c", "--convert-link", "-np", "--page-requisites", "-erobots=off", '''--tries="inf"''', "--wait=0.6", "--random-wait", '''--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0"''', current_page_url]
+    
+    ## Call Wget command
 	subprocess.call(wget_command_list)
+    
+    ## Pause 0.5 to 1.5 seconds
 	time.sleep(0.5 + random.random())
+    
+    ## Update pagination variable
 	skip_value += 20
--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 - demo by Steve McLaughlin
 - [https://iffybooks.net](https://iffybooks.net)
+- [https://post.lurk.org/@iffybooks](https://post.lurk.org/@iffybooks)
  
+➡️ [Part 1: Web scraping with Wget](https://github.com/stevemclaugh/digital-ethnography-demo/blob/main/01_Wget/LiveJournal_Backup_with_Wget.md)
+
+➡️ [Part 2: Web scraping with Python + Wget](https://github.com/stevemclaugh/digital-ethnography-demo/blob/main/02_Python_and_Wget/LiveJournal_Backup_with_Python_and_Wget.py)
+
+➡️ [Part 3: Web scraping with Python + Selenium](https://github.com/stevemclaugh/digital-ethnography-demo/blob/main/03_Python_and_Selenium/Instagram_Web_Scraping_Demo.ipynb)
